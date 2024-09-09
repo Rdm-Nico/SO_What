e4050d6ef206b9b706e6c2948432771aff3d6a3e
nicorossi2810 <272179@studenti.unimore.it>
2024-09-09 10:54:27 +0200

    change visibility of config

--- api/utils/config/config_auth.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    SECRET_KEY: "e194418fc2ddfc3fe9a25d79257af8dbd6d3d457d002afeb6530518241703cf9",
-    jwtExpiration: 3600, // 1 hour
-    jwtRefreshExpiration: 86400 // 24 hours
-};
--- api/utils/config/config_token.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    NAME_SESSION: 'api-session',
-    SECRET_KEYS: ["0761c5b6331fe707e70f6598a369b27b74cf56dd3fd687d3bb7048ccd91d3fe3","3e420e8205714b1548a19f43d04018abf11e526fb3ee843524f7ad0bf6f5b686"],
-    HTTP_ONLY: true,
-    MAX_AGE: 8 * 60 * 60 * 1000, // 8 hours
-};
